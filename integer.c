7c4e2ebb45f3848ea5d774b4dfbf15afe74409e0
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-12 21:16:02 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int intValue;
+    float floatValue;
+    printf("Enter an integer:");
+    scanf("%d",&intValue);
+    printf("Enter a float");
+    scanf("%f",&floatValue);
+    printf("you entered the integer:%d\n",intValue);
+    printf("you entered the float :%f\n",floatValue);
+}
