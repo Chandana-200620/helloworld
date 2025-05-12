f32e5b4500b02d3dfbf39174fcfb37bd2382b70f
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-12 16:07:35 +0530

    Create userdetails.c

---
+++ userdetails.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int age,usn;
+    float weight,height;
+    char name,college;
+    printf("Enter the age,usn,weight,height,name and college\n");
+    scanf("%d",&age);
+    scanf("%d",&usn);
+    scanf("%f",&weight);
+    scanf("%f",&height);
+    scanf("%s",&name);
+    scanf("%s",&college);
+    printf("=============");
+}
