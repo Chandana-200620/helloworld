347513e915bc17ecbafddd68d854112b620b9a7a
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-12 21:01:58 +0530

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char name[]="joe";
+    int age=25;
+    char location[]="japan";
+    printf("Name:%s\n",name);
+    printf("Age:%d\n",age);
+    printf("location:%s\n",location);
+    return 0;
+}
