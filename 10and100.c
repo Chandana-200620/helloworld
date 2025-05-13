ecc781fe28620d31ac0180dfa7db79f7426b372f
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 21:59:53 +0530

    Create 10and100.c

---
+++ 10and100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int n;
+int main() {
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n>=10&&n<=100){
+      printf("the number is between 10 and 100",n);
+    }
+      
+  }
+  Enter the number:26
+the number is between 10 and 100
