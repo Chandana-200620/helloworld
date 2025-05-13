7614284d3c1030602901b5a1f7e71e6aa43cc87a
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 16:01:13 +0530

    Create greaternum.c

---
+++ greaternum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int a,b;
+int main(){
+    printf("Enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("a is greater");
+    }
+        else{
+            printf("b is greater");
+        }
+        
+    }
+Enter the value of a and b:10 15
+b is greater
