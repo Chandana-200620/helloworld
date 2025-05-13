cfe2c4cbcd5340e634ae85884a73708e73f54c18
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 15:25:13 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int add(int a,int b)
+{
+int c=a+b;
+return c;
+}
+//function declaration
+
+int main() {
+      // function calling 
+     int result=add(5,10);
+     printf("the result is:%d",result);
+      return 0;
+      
+
+    
+}
+the result is:15
