d9ad1cfcbf765fe37ff34c4b9c12320ea0ed8504
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 22:04:19 +0530

    Create largestof3.c

---
+++ largestof3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int a=10,b=6,c=9;
+int main() {
+    if(a>b){
+        printf("b is smaller");
+    }
+    else if(b>c){
+        printf("c is smaller");
+    }
+    else{
+    printf("b is smaller");
+    
+    }
+     
+    }
+      
+  b is smaller
