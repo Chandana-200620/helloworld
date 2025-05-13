abce98fa8258b4f9eb75f777aa9c5dfb46fd688c
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 11:04:39 +0530

    Create 123.c

---
+++ 123.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    char name[20];
+    printf("Enter your name:");
+    scanf("%s",&name);
+    printf("Enter your age:");
+    scanf("%d",age);
+    printf("Hello %s ,you are %d years old",name,age);
+    return 0;
+     
+}
