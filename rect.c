9021746af23eea9a2c85f6ad4da41bf5349012fe
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 10:33:40 +0530

    Create rect.c

---
+++ rect.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int l,h,a;
+    printf("Enter the value of l and h:\n");
+    scanf("%d%d",&l,&h);
+    a=l*h;
+    printf("Area of rectangle is %d",a);
+
+}
+Enter the value of l and h:
+5 5
+Area of rectangle is 25
