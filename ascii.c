40dec1cb54b82cb880fa978ee9a23baad23413f4
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 10:38:43 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    printf("The ASCII value of %c is %d\n",ch,ch);
+    
+}
+Enter a character:a
+The ASCII value of a is 97
