aafaa263997b3702b1228ded69f698eea71d590f
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 10:08:54 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    if(a>0)
+    printf("The number %d is positive");
+}
+Enter the value of a:2
+The number 2 is positive
