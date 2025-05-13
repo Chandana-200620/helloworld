1ace8fd416586aa1f7d70d89d77622a029b62d60
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 16:08:34 +0530

    Create circle.c

---
+++ circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int r,a;
+float Pi=3.14;
+int main(){
+    printf("Enter the value of r:");
+    scanf("%d",&r);
+    a=Pi*r*r;
+    printf("The area of circle is %d",a);
+    
+    
+        
+    }
+Enter the value of r:3
+The area of circle is 28
