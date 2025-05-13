3c311eb53f922e3f31442194b3b8ad2530c5d933
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 10:42:01 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+     int a,b;
+     printf("Enter the value of a and b:\n");
+     scanf("%d%d",&a,&b);
+     a+b;
+     printf("Sum=%d",a+b);
+}
+Enter the value of a and b:
+5 6
+Sum=11
