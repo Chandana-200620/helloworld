3022e68c517860ca488c256949d52aac6b321d6c
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-13 15:39:18 +0530

    Create details.c

---
+++ details.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int shridevi()
+{
+printf("Chandana\n");
+printf("20-06-2006\n");
+printf("19\n");
+printf("8904191292\n");
+
+}
+int main(){
+   shridevi();
+    return 0;
+}
+Chandana
+20-06-2006
+19
+8904191292
