af0b51349fdd3450031317efa09a5c7ad8fa76dc
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-14 19:22:54 +0530

    Create circlefun.c

---
+++ circlefun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+float circle(float r,float p){
+    float area=p*r*r;
+    return area;
+}
+int main(){
+    float r=2;
+    float p=3.14;
+    float area=circle(r,p);
+    printf("The area  is:%f",area);
+    return 0;
+}
+The area  is:12.560000
