de8d37ad9aa9763b34870abcc54513c2da44a6fa
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-14 18:47:57 +0530

    Create sq.c

---
+++ sq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int sq(int n){
+    return n*n;
+    
+}
+int main(){
+    int n=5;
+    int result=sq(n);
+    printf("The square of %d is %d\n",n,result);
+    return 0;
+}
+    The square of 5 is 25
