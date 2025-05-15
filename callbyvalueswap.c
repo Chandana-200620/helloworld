59f826939753d06e8a72cfde76339148f0e659ce
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-15 13:07:44 +0530

    Create callbyvalueswap.c

---
+++ callbyvalueswap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=20;
+    int p1=a;
+    int p2=b;
+    printf("Before swap\n");
+    printf("a=%d\n",p1);
+    printf("b=%d\n",p2);
+    int temp=p1;
+    p1=p2;
+    p2=temp;
+    printf("After swap\n");
+    printf("a=%d\n",p1);
+    printf("b=%d\n",p2);
+    
+}
+Before swap
+a=10
+b=20
+After swap
+a=20
+b=10
