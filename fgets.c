151bfcb8ca934d0b7cc47e929948b256dcfb0075
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-15 18:44:50 +0530

    Create fgets.c

---
+++ fgets.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char name[10];
+    printf("Enter your name:");
+    fgets(name,10,stdin);
+    printf("your name is : %s",name);
+    
+
+}
+Enter your name:Chandana
+your name is : Chandana
