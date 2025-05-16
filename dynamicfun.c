369dd706d8188ea71a2a55c312b2cab8ced06ace
Chandana-200620 <chandanalokesh604@gmail.com>
2025-05-16 15:28:28 +0530

    Create dynamicfun.c

---
+++ dynamicfun.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr=(int*)malloc(3*sizeof(int));
+    ptr[0]=10;
+    ptr[1]=20;
+    ptr[2]=30;
+    ptr=(int*)realloc(ptr,5*sizeof(int));
+    ptr[3]=40;
+    ptr[4]=50;
+    
+    for(int i=0;i<5;i++)
+    printf("%d ",ptr[i]);
+free(ptr);
+}
+10 20 30 40 50 
